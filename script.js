711cbddc40e506954d0955d01a4b62d6126f4a53
Alejandro Rojas <cidorcorojas@gmail.com>
2024-10-14 15:06:31 -0300

    finish the proyect

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const container = document.getElementById('container')
 
-let data;
-
+let images;
 const getData = async ()=> {
     try {
-        const res = await fetch('https://dog.ceo/api/breed/hound/images/random/10')
+        const res = await fetch('https://dog.ceo/api/breed/hound/images/random/50')
         const response = await res.json();
-        data = response;
+        return response
     } catch(error) {
         console.error('Error:', error)
     }
 }
 getData().
-then(()=> console.log(data))
+then((data)=> {
+    images = data.message.map((image)=> {
+        return `<span class="rounded overflow-hidden m-2"><img class="images w-100 h-100" src=${image} alt="dog"/></span>`
+    })
+    container.innerHTML = images.join('')
+})
